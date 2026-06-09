cc89c802259994d64231b16a419cc1a4ff94adaa
HeoJunSeo <junso0324@naver.com>
2026-06-09 17:22:56 +0900

    MVI & viewmodel

--- lib/presentation/list/list_action.dart
+++ lib/presentation/list/list_action.dart
@@ -4,7 +4,7 @@ part 'list_action.freezed.dart';
 
 @freezed
 sealed class ListAction with _$ListAction {
-  const factory ListAction.onChangeQuery() = OnChangeQuery;
-  const factory ListAction.onClickSearch(String query) = OnClickSearchIcon;
-  const factory ListAction.onClickPhotoItem() = OnClickPhotoItem;
+  const factory ListAction.onChangeQuery(String query) = OnChangeQuery;
+  const factory ListAction.onClickSearch() = OnClickSearchIcon;
+  const factory ListAction.onClickPhotoItem(int id) = OnClickPhotoItem;
 }
