4f916b3ff1e2ef1d23e385c54d41053137a283ee
HeoJunSeo <junso0324@naver.com>
2026-06-09 17:22:36 +0900

    MVI

---
+++ lib/presentation/list/list_view_model.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:image_search_app/core/result.dart';
+import 'package:image_search_app/domain/model/photo.dart';
+import 'package:image_search_app/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search_app/presentation/list/list_action.dart';
+import 'package:image_search_app/presentation/list/list_event.dart';
+import 'package:image_search_app/presentation/list/list_state.dart';
+
+class ListViewModel extends ChangeNotifier {
+  final GetPhotosUseCase _getPhotosUseCase;
+
+  ListState _state = const ListState();
+  ListState get state => _state;
+
+  final _eventController = StreamController<ListEvent>();
+  Stream<ListEvent> get eventStream => _eventController.stream;
+
+
+  ListViewModel({required GetPhotosUseCase getPhtoUseCase})
+    : _getPhotosUseCase = getPhtoUseCase;
+
+  Future<void> onAction(ListAction action) async {
+    switch (action) {
+      case OnChangeQuery():
+        _state = state.copyWith(query: action.query);
+        notifyListeners();
+      case OnClickSearchIcon():
+        _state = state.copyWith(isLoading: true);
+        notifyListeners();
+
+        final result = await _getPhotosUseCase.execute(state.query);
+        switch (result) {
+          case Success<List<Photo>>():
+            _state = state.copyWith(photos: result.data, isLoading: false);
+            notifyListeners();
+          case Error<List<Photo>>():
+            _eventController.add(ListEvent.showSnackbar('뭔가 에러'));
+        }
+      case OnClickPhotoItem():
+        print(action.id);
+    }
+  }
+}
